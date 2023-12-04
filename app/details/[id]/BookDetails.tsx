5fe694408c3d1950a87aecb613faac0fbd821641
Daniel Tulpa <dtulpa16@yahoo.com>
2023-12-04 09:38:23 -0700

    Added Styles To Book Details Page

--- app/details/[id]/BookDetails.tsx
+++ app/details/[id]/BookDetails.tsx
@@ -1,3 +1,4 @@
+import { supabaseServerClient } from "@/app/lib/initSupabase";
 import { BookVolume } from "@/app/lib/types";
 import React from "react";
 
@@ -5,17 +6,51 @@ type BookDetailsProps = {
   book: BookVolume;
 };
 
-export default function BookDetails({ book }: BookDetailsProps) {
+export default async function BookDetails({ book }: BookDetailsProps) {
+  const {
+    data: { user },
+  } = await supabaseServerClient.auth.getUser();
   return (
-    <div>
-      <h1>{book.volumeInfo.title}</h1>
-      <h2>{book.volumeInfo.authors[0]}</h2>
-      <h3 dangerouslySetInnerHTML={{ __html: book.volumeInfo.description }} />
-      <img src={book.volumeInfo.imageLinks.thumbnail} />
-      <form>
-        <input className="hidden" value={book.id} />
-        <button type="submit">Add To Favorites</button>
-      </form>
+    <div className="bg-white p-6 md:p-10 lg:p-12 max-w-5xl mx-auto rounded-lg border border-gray-200 shadow-md">
+      <div className="flex flex-col md:flex-row">
+        <img
+          src={book.volumeInfo.imageLinks.thumbnail}
+          alt={book.volumeInfo.title}
+          className="md:w-1/3 object-cover rounded mr-4 mb-4 md:mb-0"
+        />
+        <div className="md:w-2/3">
+          <h1 className="text-2xl font-bold text-gray-800 mb-2">
+            {book.volumeInfo.title}
+          </h1>
+          <h2 className="text-xl text-gray-600 mb-4">
+            {book.volumeInfo.authors.join(", ")}
+          </h2>
+          <div
+            dangerouslySetInnerHTML={{ __html: book.volumeInfo.description }}
+            className="prose mb-4"
+          />
+          <form
+            action={async () => {
+              "use server";
+              await supabaseServerClient.from("favorites").insert([
+                {
+                  bookId: book.id,
+                  title: book.volumeInfo.title,
+                  thumbnail_url: book.volumeInfo.imageLinks.thumbnail,
+                  reader: user?.id,
+                },
+              ]);
+            }}
+          >
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
+            >
+              Add To Favorites
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
--- app/details/[id]/page.tsx
+++ app/details/[id]/page.tsx
@@ -11,7 +11,7 @@ export default async function DetailsPage({
     `https://www.googleapis.com/books/v1/volumes/${params.id}`
   );
   const book: BookVolume = await data.json();
-  console.log("Book: ", book);
+
   return (
     <div>
       <BookDetails book={book}/>
